33a86c2fc6d4492fdf05e2837138d79f87831981
Breyanna Vass <breyannavass@gmail.com>
2019-10-14 19:43:26 -0400

    Adding information from firebase to create the calendar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDMEd3umQp7ktPIUvgGcAemRBAL4-P-jD4",
+  authDomain: "vue-calender-52a5b.firebaseapp.com",
+  databaseURL: "https://vue-calender-52a5b.firebaseio.com",
+  projectId: "vue-calender-52a5b",
+  storageBucket: "vue-calender-52a5b.appspot.com",
+  messagingSenderId: "421318369427",
+  appId: "1:421318369427:web:6b1b4e9c0d352b3492ad66"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
